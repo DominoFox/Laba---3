862114fa9928d41b857379c1241ea290b00518ba
DominoFox <domino.fox.192@gmail.com>
2022-06-17 23:07:21 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/GoldenSection.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class GoldenSection
+    {
+        public delegate double Function(Point x);
+        public double eps { get; set; } = 0.01;
+        private double fi;
+        public long MaxSteps = 10000;
+        public long Steps { get; set; }
+        public GoldenSection()
+        {
+            fi = (1 + Math.Sqrt(5)) / 2;
+        }
+        public double Optimize(Func<Point, double> f, Point vars, int varIndex, double a, double b)
+        {
+            double x1, x2, y1, y2;
+            do
+            {
+                x1 = b - (b - a) / fi;
+                x2 = a + (b - a) / fi;
+                vars[varIndex] = x1;
+                y1 = f(vars);
+                vars[varIndex] = x2;
+                y2 = f(vars);
+                if (y1 >= y2) a = x1;
+                else b = x2;
+                Steps++;
+                if (Steps > MaxSteps) break;
+
+            }
+            while (Math.Abs(b - a) > eps);
+            return (b + a) / 2;
+        }
+
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static double f(Point point) => point[0] + point[1] - ((point[0] + point[1]) * (point[0] + point[1])) - (4 * point[1] * point[1]);
+        static Point GaussMin(Func<Point, double> f, Point start, double pr = 0.01, double g = 1)
+        {
+            Point uTemp = start;
+            double I1, I2;
+            int size = start.length;
+            double gr;
+            double h = 1e-6;
+
+            for (int k = 1; k < 1000; k++)
+            {
+                double checkSum = 0;
+                Point prevPoint = uTemp;
+                for (int i = 0; i < size; i++)
+                {
+                    Point stepPoint = uTemp;
+                    stepPoint[i] = stepPoint[i] + h;
+                    I1 = f(stepPoint);
+
+                    stepPoint = uTemp;
+                    stepPoint[i] = stepPoint[i] - 2 * h;
+                    I2 = f(stepPoint);
+
+                    gr = (I1 - I2) / (2 * h);
+
+                    uTemp[i] = uTemp[i] - g * gr;
+
+                    checkSum += (prevPoint[i] - uTemp[i]) * (prevPoint[i] - uTemp[i]);
+
+                    if(i == 0) Console.WriteLine($"X: \t {uTemp[i]}");
+                    if(i == 1) Console.WriteLine($"Y: \t {uTemp[i]}\n");
+                    Console.WriteLine($"({prevPoint[i]} - {uTemp[i]}) * ({prevPoint[i]} - {uTemp[i]}) \t {checkSum}\n");
+
+                }
+                if (Math.Sqrt(checkSum) <= pr && k > 1)
+                {
+                    Console.WriteLine($"Done.Number of steps: { k}");
+                    break;
+                }
+            }
+            return uTemp;
+        }
+        static void Main(string[] args)
+        {
+            var res = GaussMin(f, new Point(new double[] {0, 0 }));
+            for (int i = 0; i < res.pointValue.Length; i++)
+            {
+                Console.WriteLine($"{res[i]}; \n");
+            }
+            Console.ReadKey();
+        }
+    }
+}
